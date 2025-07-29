d5fb6a3572f7003d280eee6d99b216b2f05e6791
AbrahamTrenyan <77699908+AbrahamTrenyan@users.noreply.github.com>
2025-07-28 21:30:00 -0300

    testear gdb

---
+++ clase1/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define CANTIDAD 10
+#define DESDE 0
+#define HASTA 10
+int main(int argc, char *argv[])
+{
+int desde = DESDE;
+int hasta = HASTA;
+int n,num;
+
+if(argc==3)
+{
+desde = atoi(argv[1]);
+hasta = atoi(argv[2]);
+}
+printf("cantidad = %d desde = %d hasta = %d \n",CANTIDAD, desde, hasta);
+srand(time(NULL));
+
+for (n=0; n<CANTIDAD;n++)
+{
+num = (rand()%(hasta-desde+1))+desde;
+printf("el numero es %d \n", num);
+}
+
+return 0;
+}
